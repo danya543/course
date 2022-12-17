ed94e66a8c8a0bcff3099c205152b74dc199ce45
danya543 <dgilev75@gmail.com>
2022-12-17 20:32:24 +0300

    main

---
+++ dist/index.672d4772.js
@@ -0,0 +1,14 @@
+new Swiper(".mentor-slider", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true
+    }
+});
+function movel() {
+    document.getElementById("img").className = "classnamel";
+}
+function mover() {
+    document.getElementById("img").className = "classnamer";
+}
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ script.js
@@ -0,0 +1,13 @@
+new Swiper('.mentor-slider',{
+    scrollbar:{
+        el:'.swiper-scrollbar',
+        draggable:true
+    },
+});
+function movel() {
+    document.getElementById('img').className = 'classnamel';
+    
+  }
+function mover() {
+  document.getElementById('img').className = 'classnamer';
+}
